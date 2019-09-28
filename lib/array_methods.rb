3b8a56521518e5a6b0a3420714b907ade0fcb17d
allison cortez <allizetorc@gmail.com>
2019-09-28 21:52:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,17 +9,18 @@ nil
 def find_max_value(array)
   x = array[0]
   array.length.times do |index|
-  if array[index] > x
-    x = array[index]
+    if array[index] > x
+     x = array[index]
   end
 end
 x
 
 def find_min_value(array)
-    x = array[0]
+  x = array[0]
   array.length.times do |index|
-  if array[index] < x
-    x = array[index]
-  end
+    if array[index] < x
+      x = array[index]
+    end
 end
 x
+end
