f5139dc3d8cd3eeba0fe0caddfc91ee8958e9504
allison cortez <allizetorc@gmail.com>
2019-09-28 21:44:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,10 @@ def find_element_index(array, value_to_find)
 array.length.times do |count|
   if array[count] == value_to_find
     return count
-end
+  end
 end
 nil
+
 def find_max_value(array)
   x = array[0]
   array.length.times do |index|
