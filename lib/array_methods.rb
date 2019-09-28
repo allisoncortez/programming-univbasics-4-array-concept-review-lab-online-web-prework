d2a56d9d178b24165963206a6658324033fbc33c
allison cortez <allizetorc@gmail.com>
2019-09-28 21:56:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   count = 0
-array.length.times do |count|
+while count < array.length do |count|
   if array[count] == value_to_find
     return count
     count +=1
